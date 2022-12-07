4dfdcb2bb1bbfa7e5947de9e135f98503695855d
devcomfort <kimdonghyun026@gmail.com>
2022-12-07 19:12:54 +0900

    RowResult 컴포넌트 추가 및 적용

---
+++ src/components/SearchResult/RowResult.jsx
@@ -0,0 +1,18 @@
+// @ts-check
+import React from "react";
+import styles from "./index.module.css";
+
+/**
+ * @typedef {object} Props
+ * @property {import("../../assets/Search").FreeDictionaryResult} result
+ */
+
+/**
+ * 테이블 '열' 컴포넌트
+ * @type {React.FC<React.PropsWithChildren>}
+ */
+const RowResult = function ({ children }) {
+	return <div className={`grid ${styles["grid-color"]}`}>{children}</div>;
+};
+
+export default RowResult;
--- src/components/SearchResult/index.jsx
+++ src/components/SearchResult/index.jsx
@@ -8,6 +8,7 @@ import AudioControl from "./AudioControl";
 import CollapsibleCard from "../CollapsibleCard";
 
 import styles from "./index.module.css";
+import RowResult from "./RowResult";
 
 /**
  * @typedef {object} Props
@@ -16,54 +17,46 @@ import styles from "./index.module.css";
 
 /** @type {React.FC<Props>} */
 const SearchResult = function ({ result }) {
-	if (result.title) {
+	const [collapsibleStates, setCollapsibleStates] = useState([]);
+
+	if (result.title)
 		return (
-			<>
-				<p>
-					정보 없음
-					<br />
-					{`(${result.title})`}
-				</p>
-			</>
+			<RowResult>
+				<span>정보 없음</span>
+				{`(${result.title})`}
+			</RowResult>
 		);
-	}
 
 	if (!(result instanceof Array))
 		return (
-			<>
-				<p>알 수 없는 자료형</p>
-			</>
+			<RowResult>
+				<span>알 수 없는 자료형</span>
+			</RowResult>
 		);
 
 	if (result.length === 0)
 		return (
-			<>
-				<p>결과 없음</p>
-			</>
+			<RowResult>
+				<span>결과 없음</span>
+			</RowResult>
 		);
 
 	return (
 		<table className="result-table">
-			<div className={`grid ${styles["grid-color"]}`}>
+			<RowResult>
 				<span>결과 번호</span>
 				<span>단어</span>
 				<span>발음 기호 IPA</span>
 				<span>음성</span>
-			</div>
+			</RowResult>
 
 			{/* 결과 정보 */}
 			{result.map((d, i) => {
 				const { meanings, phonetics, word } = d;
 
-				/** @template {boolean} collapsible 활성 상태 */
-				const [isActive, setIsActive] = useState(false);
-
 				return (
 					<div>
-						<div
-							className={`grid ${styles["grid-color"]}`}
-							onClick={(e) => setIsActive(!isActive)}
-						>
+						<RowResult>
 							<span>{i + 1}</span>
 							<span>{word}</span>
 							<span
@@ -97,8 +90,8 @@ const SearchResult = function ({ result }) {
 									})
 								)}
 							</span>
-						</div>
-						<CollapsibleCard isActive={isActive}>
+						</RowResult>
+						<CollapsibleCard isActive={false}>
 							<table>
 								<MeaningTable meanings={meanings}></MeaningTable>
 							</table>
