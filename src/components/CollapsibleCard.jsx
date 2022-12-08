cf39142865ccedc853065d83c1445d89e597f715
devcomfort <kimdonghyun026@gmail.com>
2022-12-08 09:08:36 +0900

    Collapsible Widget 애니메이션 삽입 (with useRef)

--- src/components/CollapsibleCard.jsx
+++ src/components/CollapsibleCard.jsx
@@ -1,5 +1,7 @@
 // @ts-check
 import React from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 import CollapsibleCardCSS from "./CollapsibleCard.module.css";
 
@@ -11,11 +13,24 @@ import CollapsibleCardCSS from "./CollapsibleCard.module.css";
 
 /** @type {React.FC<Props>} */
 const CollapsibleCard = ({ isActive, children }) => {
+	// ref hook 기본값 선언
+	const ref = useRef(null);
+
+	useEffect(() => {
+		ref.current.style = isActive
+			? `max-height: ${ref.current.scrollHeight}px`
+			: `max-height: 0px`;
+	}, [isActive]);
+
 	return (
 		<div
 			className={`${CollapsibleCardCSS.card} ${
 				isActive ? CollapsibleCardCSS.active : ""
 			}`}
+			// 내부 속성 수정을 위한 ref hook 데이터
+			ref={ref}
+			// collapsible widget 클릭으로 인한 외부 이벤트 실행 방지
+			onClick={(e) => e.stopPropagation()}
 		>
 			{children}
 		</div>
